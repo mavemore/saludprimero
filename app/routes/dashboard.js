0e955f85ce9be8696630ccbf88f197cf60b602da
mavemore <veronica940413@hotmail.com>
2016-08-10 14:26:39 -0500

    Creados archivos views
    Solo est'an creados para saber cuales son de hacer, se arregl'o un
    problema que habia al momento de ver en disposititvos m'oviles.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+//este script es solo de prueba, para ver los views
 router.get('/', function(req, res, next) {
   res.render('dash_layout', { title: 'Bienvenido' });
 });
 
 router.get('/usuario', function(req, res, next) {
-  res.render('dash_user', { title: 'Bienvenido' });
+  res.render('usuario/dash_user', { title: 'Bienvenido' });
 });
 
 router.get('/operario', function(req, res, next) {
-  res.render('dash_operario', { title: 'Bienvenido' });
+  res.render('operario/dash_operario', { title: 'Bienvenido' });
 });
 
 router.get('/laboratorista', function(req, res, next) {
-  res.render('dash_lab', { title: 'Bienvenido' });
+  res.render('laboratorista/dash_lab', { title: 'Bienvenido' });
 });
 
 module.exports = router;
