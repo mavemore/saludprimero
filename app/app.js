6b38ba6caa94b66ba643d985ac1b8155be11bd26
mavemore <veronica940413@hotmail.com>
2016-08-21 17:03:53 -0500

    Home usuario con fotos

--- app/app.js
+++ app/app.js
@@ -49,6 +49,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
+
 //oscar
 app.use(session({
   secret: 'secret',
