55e73305e14025192b4549f4122a965afd357136
CarlosMH95 <cmanosalvas95@gmail.com>
2016-08-23 22:44:54 -0500

    Correccion 2.0

--- app/routes/operario.js
+++ app/routes/operario.js
@@ -97,7 +97,7 @@ router.post('/ingreso-muestras/nuevoPaciente',  function (req, res, done) {
             newUser.email = email;
             var pass = "";
             var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            for( var i=0; i < 5; i++ ) {
+            for( var i=0; i < 8; i++ ) {
                 pass += possible.charAt(Math.floor(Math.random() * possible.length));
             }
 
